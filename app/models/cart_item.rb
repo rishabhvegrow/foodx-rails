5711e084f01b6b262d895b55e8a1460e4bbf7502
Rishabh Tripathi <rishabh@192.168.1.6>
2023-11-06 21:13:28 +0530

    basic scope validations and callbacks

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,15 @@
 class CartItem < ApplicationRecord
+    belongs_to :user
+
+    scope :checked_out, -> { where(is_checked_out: true) }
+
+    validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    alidates :price, presence: true, numericality: { greater_than: 0 }
+
+    before_save :calculate_total_price
+
+    def calculate_total_price
+        self.total_price = quantity * price
+    end
+
 end
